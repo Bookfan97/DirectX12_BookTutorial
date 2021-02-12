7cc7d881cf00cc57b55832b1717c00cf31cbe9f0
Noah Day <sheltielover432@gmail.com>
2021-02-12 01:20:00 -0500

    Initialization

---
+++ DirectX12_BookTutorial/InitDirect3DApp.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "d3dApp.h"
+
+class InitDirect3DApp : public D3DApp
+{
+public:
+	InitDirect3DApp(HINSTANCE hInstance);
+	~InitDirect3DApp();
+	virtual bool Initialize()override;
+
+	void Start();
+
+private:
+    virtual void OnResize()override;
+    virtual void Update(const GameTimer& gt)override;
+    virtual void Draw(const GameTimer& gt)override;
+
+};
